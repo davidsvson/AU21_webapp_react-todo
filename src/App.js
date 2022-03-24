90a8b608642d78f588807f5d719c3d148517d4e3
David Svensson <davidsvson@gmail.com>
2022-03-24 14:06:15 +0100

    1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+
+const DoneButton = (props) => {
+  const [done, setDone ] = useState(props.done);
+
+  let todoDone = 'Ej klar';
+  if (done === true) {
+    todoDone = 'Klar';
+  }
+
+  return (
+    <button onClick={() => setDone(!done) } > {todoDone} </button>
+  )
+
+}
+
+const ToDoItem = (props) => {
+
+  return (
+    <li className="todo-item">{props.text}  <DoneButton done={props.done} /> </li>
+  )
+}
+
+const AddItem = (props) => {
+  const [input, setInput] = useState('');
+
+  const addAndClear = () => {
+    props.handleAdd(input);
+    setInput('');
+  }
+
+  return (
+    <div>
+      <input type="text" value={input} onInput={e => setInput(e.target.value) } />
+      <button onClick={ addAndClear }>Add</button>
+    </div>
+  )
+}
+
 
 function App() {
+  // const item = {text: 'köp ost', done: false};
+  // const defaultValue = [item]
+  const [todoList, setTodoList] = useState([{text: 'köp ost', done: false}]);
+
+  const todoItems = todoList.map((item, index) => (
+    <ToDoItem text={item.text} done={item.done} />
+  ))
+
+  const addItem = (itemText) => {
+    const newItem = {text: itemText, done: false};
+    setTodoList([...todoList, newItem ]);
+  }
+
   return (
-    <div className="App">
+     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Todo app</h1>
       </header>
+      <AddItem handleAdd={addItem}/>
+      <ul className="todo-list">
+          {todoItems}
+      </ul>  
     </div>
   );
 }
